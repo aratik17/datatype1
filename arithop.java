dd16ad6a687a91144a65cf669ef87a6a5e2b5161
artaik17 <aratikundargi17@gmail.com>
2025-04-09 04:56:34 +0530

    save this file

---
+++ arithop.java
@@ -0,0 +1,13 @@
+package JAVA;
+
+public class arithop {
+    public static void main(String[] args){
+        int a=10;
+        int b=30;
+        System.out.println("a+b:" +(a+b));
+        System.out.println("a-b:" +(a-b));
+        System.out.println("a*b:" +(a*b));
+        System.out.println("a/b:" +(a/b));
+        System.out.println("a%b:" +(a%b));
+    }
+}
